2129e2498e2e43abcdbb470601fd1053c1011319
Aaron 'Da Possession Ends Here' Zhang <aarondzhang33@gmail.com>
2024-02-11 18:00:32 -0800

    fixing

--- utils/realmApp.js
+++ utils/realmApp.js
@@ -1,9 +1,10 @@
 const Realm = require('realm-web')
 const {APPID} = require("./environment")
 
-console.log(APPID)
 const app = new Realm.App({
     id: APPID,
 });
+
+//https://scbites-server-ae26174ba77b.herokuapp.com/
 
 module.exports = app;
