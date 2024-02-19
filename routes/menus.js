ddc51cd884c61ec9e8c2070a8ea0577f97fec951
Aaron 'Da Possession Ends Here' Zhang <aarondzhang33@gmail.com>
2024-02-19 11:36:49 -0800

    buildpack issues

--- routes/menus.js
+++ routes/menus.js
@@ -119,7 +119,12 @@ async function sendEmails() {
 
 
     let browser;
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+        'args' : [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
+    });
     const [page] = await browser.pages();
 
     for (let user of userData) {
