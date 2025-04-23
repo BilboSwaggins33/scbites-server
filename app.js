1050bdb7fa93ecccb29a16f6eb849453f9c9a019
aaronzhang <aarondzhang33@gmail.com>
2025-04-23 15:07:59 -0700

    remove ./routes/test

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const authRouter = require("./routes/auth");
-const testRouter = require("./routes/test");
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -17,7 +16,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/auth", authRouter);
-app.use("/test", testRouter);
 
 const Realm = require("realm-web");
 const { APPID } = require("./utils/environment");
@@ -25,11 +23,11 @@ const { APPID } = require("./utils/environment");
 const realmApp = new Realm.App({
   id: APPID,
 });
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
